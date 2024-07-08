785a1767ee1e1a24d4efc0a6a6c843e08ea2badd
Caio Bakerolov <caiobakerolov@icloud.com>
2024-07-08 16:01:23 +1000

    a little adjust

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,13 +1,11 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Meals from './pages/Meals'
-import Product from './pages/Product'
 
 const Rout = () => (
   <Routes>
     <Route path="/" element={<Home />} />
     <Route path="/meals" element={<Meals />} />
-    <Route path="/product" element={<Product />} />
   </Routes>
 )
 
